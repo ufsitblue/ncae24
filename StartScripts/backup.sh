f5f35e5faa31e86928cdf382dfa55693f15f7be9
Andrey Nikitin <143126464+pyukey@users.noreply.github.com>
2024-03-06 11:20:42 -0500

    Create backup.sh
    Use this script for backups

---
+++ StartScripts/backup.sh
@@ -0,0 +1,9 @@
+diff -r /etc "$1:/opt/backups/$2/etc"
+diff -r /var/www "$1:/opt/backups/$2/var/www"
+diff -r /var/www-html "$1:/opt/backups/$2/var/www-html"
+
+if [ $# -eq 3 ]; then
+  rsync -av -e ssh /etc "$1:/opt/backups/$2"
+  rsync -av -e ssh /var/www "$1:/opt/backups/$2"
+  rsync -av -e ssh /var/www-html "$1:/opt/backups/$2"
+fi
